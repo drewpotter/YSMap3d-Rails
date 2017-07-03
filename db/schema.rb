e1806fb40f0795c432ddf0875b3ac9d5d62fdcfc
Drew Potter <hello@drewpotter.com>
2017-07-03 14:37:10 +0100

    Added logo and search box, but still need to do the CSS also added routes for Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703124320) do
+ActiveRecord::Schema.define(version: 20170703132225) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
